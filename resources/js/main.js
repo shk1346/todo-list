22f07e059fd1e84c7e337cf24b500df8bc349f48
shk1346 <89139639+shk1346@users.noreply.github.com>
2021-08-22 12:22:41 +0900

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -25,7 +25,7 @@ let renderCurrentTimer = () => {
 	1. placeholder의 문구를 바꿔준다. 
 	2. eventListener 우리가 입력한 데이터를 화면에 뿌려주고, localStorage에 저장	
 */
-let renderUser = (evnet) => {
+let renderUser = (event) => {
 	
 	//HTML요소의 기본 이벤트 중지 
 	event.preventDefault();
@@ -41,9 +41,9 @@ let renderUser = (evnet) => {
 //일정 등록하는 함수(데이터추가)
 let registSchedule = (event) => {
 	
-	//HTML요소의 기본 이벤트 중지 
+	//HTML요소의 기본 이벤트 중지 *********시험에 나올 수 있음
 	event.preventDefault();
-	
+		
 	console.dir('registSchedule : ' + event.target.className);
 	
 	let prevTodo = localStorage.getItem('todo'); //이전사용자가 todo list를 등록한 적이 있는지
@@ -55,10 +55,10 @@ let registSchedule = (event) => {
 		todoList = JSON.parse(prevTodo); //object로 변환
 		let idx = Number(localStorage.getItem('lastIdx')) + 1;
 		localStorage.setItem('lastIdx',idx); //1씩 증가하는 idx를 식별자로 가지게
-		todoList.unshift({work:input, idx:idx}); // todoList 배열 가장 처음에 추가 unshift()
+		todoList.push({work:input, idx:idx}); // todoList 배열 가장 처음에 추가 unshift(), 마지막에 추가 push()
 		
 	}else{ //아니라면 새롭게 추가
-		localStorage.setItem('lastIdx',0); //삭제기능을 추가하려면 식별자가 있어야됨. 
+		localStorage.setItem('lastIdx',0); 
 		todoList.push({work:input, idx:0});  //처음일 땐 식별자=0
 	}
 	
@@ -74,7 +74,7 @@ let registSchedule = (event) => {
 let removeSchedule = event =>{
 	let curPage = Number(document.querySelector('#currentPage').textContent);
 	let todoList = JSON.parse(localStorage.getItem('todo'));
-	/*console.dir(todoList);*/
+
 	let removedList = todoList.filter(e => {
 		return event.target.dataset.idx != e.idx; //i태그의 식별자와 같지 않은 애만 반환
 	});
@@ -96,23 +96,39 @@ let renderSchedule = (todoList) => { //생성하는 시점
 	document.querySelector('.inp_todo').value = '';
 	
 	// Your Work쪽에 뿌려질 수 있도록 해보자
-	todoList.forEach(schedule => { //e : 작업들 work들. 
+	todoList.forEach(schedule => { 
 		let workDiv = document.createElement('div'); //div생성
-		workDiv.innerHTML = `<i class="fas fa-trash-alt" data-idx=${schedule.idx}></i> ${schedule.work}`; //id를 갖다 쓸 수 있게 data-idx추가
+		workDiv.innerHTML = `<i class="fas fa-trash-alt" data-idx="${schedule.idx}"></i> ${schedule.work}`; //id를 갖다 쓸 수 있게 data-idx추가
 		document.querySelector('.todo-list').append(workDiv); //이렇게 하면 style로 값을 잡아줄 수 있음
+		
 	});
 	
+	document.querySelectorAll('.todo-list>div').forEach(e =>{
+		e.addEventListener('mouseover', ()=>{
+			e.style.backgroundColor = "grey";
+		})
+		e.addEventListener('mouseout', ()=>{
+			e.style.backgroundColor = "transparent";
+		})
+		e.addEventListener('click', ()=>{
+			e.style.textDecoration = "line-through";
+			e.style.textDecorationStyle = "double";
+			e.style.textDecorationColor = "red";
+		})
+	})
 	
 	document.querySelectorAll('.todo-list>div>i').forEach(e =>{
 		e.addEventListener('click', removeSchedule)
 	})
 }
+
 //dir : 방향
 let renderPagination = (event) =>{
+	
 	let dir = Number(event.target.dataset.dir);
 	// 미리 생각해둘 데이터들!
 	//1. 현재페이지 값 (<span>1</span>에 들어있음)
-	let curPage = Number(document.querySelector('#currentPage').textContent); //console에 document.querySelector(',currentPage') 찍어보면 데이터들 확인 가능. 그 중 textContent 가져오기 
+	let curPage = Number(document.querySelector('#currentPage').textContent); 
 	let lastPage;
 	let renderPage = curPage + dir; //dir은 -1 또는 +1 (이전페이지, 다음페이지)
 	
@@ -121,7 +137,7 @@ let renderPagination = (event) =>{
 	if(todoList){ //todoList가 존재한다면
 		todoList = JSON.parse(todoList);
 		let todoCnt = todoList.length;  
-		lastPage = Math.ceil(todoCnt/8); //마지막페이지 초기화, 데이터가 87개라면 10페이지+데이터7개. 따라서 필요한 페이지 수는 11개이므로 올림처리하자 (우리는 한 페이지당 데이터를 8개씩 뿌리는 중)
+		lastPage = Math.ceil(todoCnt/8); 
 	}
 	
 	//사용자가 마지막 페이지를 보고 있다면 다음 페이지로 넘어가면 안됨
@@ -134,10 +150,11 @@ let renderPagination = (event) =>{
 		alert('첫번째 페이지입니다.');
 		return;
 	}
+	
 	//3. 페이지당 뿌릴 데이터 숫자
 	//마지막 페이지가 아니라면 다음 페이지가 보여지도록
 	
-	let end = renderPage*8; //랜더링해야되는 페이*8만큼 
+	let end = renderPage*8; 
 	let begin = end-8;
 	
 	renderSchedule(todoList.slice(begin, end));
@@ -145,10 +162,8 @@ let renderPagination = (event) =>{
 }
 
 
-/* 입력 후 새로고침을 해도 입력한 값이 여전히 화면에 남아있어야 됨. 
-	처음 화면이 로드되는 시점에 localStorage값을 가져와서 그 값으로 판단해줘야 함.
-*/
-let convertMainDiv = (username) => { //username을 매개변수로 전달받아 뿌려주는 함수
+
+let convertMainDiv = (username) => { 
 
 	document.querySelector('.username').innerHTML = username;
 	document.querySelector('.inp_username').placeholder = 'Enter your schedule';
@@ -168,7 +183,7 @@ let convertMainDiv = (username) => { //username을 매개변수로 전달받아 
 	document.querySelector('.frm_todo').removeEventListener('submit', renderUser);//기존에 등록한 submit이벤트 제거
 	//callback함수를 호출하는 addEventListener 
 	document.querySelector('.frm_todo').addEventListener('submit', registSchedule);
-	//click이벤트가 발생했을 때 페이지 웅앵웅이 발생하도록
+	//화살표 아이콘에 click이벤트
 	document.querySelector('#leftArrow').addEventListener('click', renderPagination);
 	document.querySelector('#rightArrow').addEventListener('click', renderPagination);
 	
@@ -183,14 +198,15 @@ let convertMainDiv = (username) => { //username을 매개변수로 전달받아 
 	let username = localStorage.getItem('username');
 	let todoList = localStorage.getItem('todo');
 	
-	if(username){ //true라면 = 사용자가 등록을 진행했다면(null이 아니라면)
-		//처음부터 사용자의 이름을  username 클래스에 뿌려줘야하고
-		//placeholder값이 변경되어있어야 함 
+	console.log(username, todoList);
+	
+	if(username){ 
+
 			convertMainDiv(username);
 			
 			if(todoList){
 				todoList = JSON.parse(todoList);
-				renderSchedule(todoList.slice(0,8)); //한 페이지당 할일목록이 8개씩만 보이도록 자름
+				renderSchedule(todoList.slice(0,8)); 
 			}
 	}else{
 		document.querySelector('.frm_username').addEventListener('submit', renderUser);
